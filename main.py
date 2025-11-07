608654bd93189e3a8963f82c2ae7538209d05a8e
thuisilva <thui.r.silva@gmail.com>
2025-11-06 21:21:18 -0300

    please dont leak

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from googleapiclient.discovery import build as google_build
 import simpleaudio as sa
+import dotenv as denv
+import os 
+
+denv.load_dotenv()
+YOUTUBE_KEY = os.getenv("YOUTUBE_KEY")
+
+print("API KEY: ", YOUTUBE_KEY)
+
+
